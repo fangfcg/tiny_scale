7c6c0958ec8d6d5b7687e576efb76e1953e32a96
liangjz <liangjz15@mails.tsinghua.edu.cn>
2017-11-22 01:32:56 +0800

    添加了mocha配置

---
+++ server/test/mocha_test.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+
+describe('Array', function(){
+    it('should return -1 when the value not present', function(){
+        assert.equal(-1, [1, 2, 4].indexOf(3));
+    });
+});
