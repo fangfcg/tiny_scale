2af02f1c6e2fe33fd1cf2747a8265a53815bd9f3
fangfcg <thssss15_fangcg@163.com>
2017-11-23 03:21:38 +0800

    实现了两个socket，准备实现前端部分的整合

--- client/client/src/api/client.js
+++ client/client/src/api/client.js
@@ -3,13 +3,13 @@ var msgId = 0
 // below are some msg content for test.
 var msgType = [2, 0, 1, 0, 1]
 var msgName = ['fcg', 'a', 'fcg', 'a', 'fcg']
-var msgContent = ['fcg为您服务', 'hello', '1', '2', 'hello']
+var msgContent = ['欢迎使用客服系统', 'hello', '1', '2', 'hello']
 var serverAddress = 'http://localhost:8081'
 
 var Chat = {
   msgList: [],
-  userName: 1,
-  userId: null,
+  // userName: 1,
+  // userId: null,
   socket: null,
   status: 0, // 0: not call. 1: calling, 2: serving
   createMsg: function () {
@@ -25,9 +25,43 @@ var Chat = {
   initSock: function () {
     this.socket = io(serverAddress)
     this.socket.on('service_response', function (data) {
-      if (data === 0) {
-        console.log(1)
+      var sysmsg = this.createMsg()
+      sysmsg.type = 2
+      if (data === false) {
+        this.status = 0
+        sysmsg.msg = '请求客服失败，请重试'
+      } else {
+        this.status = 1
+        sysmsg.msg = '已收到消息，即将为您分配客服'
       }
+      this.msgList.push(sysmsg)
+    })
+    this.socket.on('operator_connected', function () {
+      this.status = 2
+      var sysmsg = this.createMsg()
+      sysmsg.msg = '已接入客服，正在为您服务'
+      sysmsg.type = 2
+      this.msgList.push(sysmsg)
+    })
+    this.socket.on('msg', function (inputMsgObj) {
+      var operatorMsg = this.createMsg()
+      operatorMsg.msg = inputMsgObj.msg
+      operatorMsg.type = 1
+      this.msgList.push(operatorMsg)
+    })
+    this.socket.on('operator_disconnected', function () {
+      this.status = 0
+      var sysmsg = this.createMsg()
+      sysmsg.msg = '客服已断开连接'
+      sysmsg.type = 2
+      this.msgList.push(sysmsg)
+    })
+    this.socket.on('crash', function () {
+      this.status = 0
+      var sysmsg = this.createMsg()
+      sysmsg.msg = '因为一些意外的错误，客服断开了连接，请稍等'
+      sysmsg.type = 2
+      this.msgList.push(sysmsg)
     })
   }
 }
--- client/operator/src/components/ChatComponent.vue
+++ client/operator/src/components/ChatComponent.vue
@@ -29,21 +29,21 @@ export default {
       msgObj.name = Chat.userName
       msgObj.msg = newMsg
       msgObj.type = 0
-      Chat.msgList.push(msgObj)
+      this.chatobj.msgList.push(msgObj)
+      this.socket.emit('msg', {msg: msgObj.msg})
     },
     callService () {
       var msgObj = Chat.createMsg()
       if (this.chatobj.status === 0) {
-        this.chatobj.status = 1
-        msgObj.msg = '已收到消息，即将为您分配客服'
+        this.chatobj.socket.emit('service_request')
       } else if (this.chatobj.status === 1) {
         msgObj.msg = '正在为您分配客服，请稍后'
-        // this.chatobj.status = 2
+        this.chatobj.msgList.push(msgObj)
       } else {
         msgObj.msg = '客服正在为您服务'
+        msgObj.type = 2
+        this.chatobj.msgList.push(msgObj)
       }
-      msgObj.type = 2
-      Chat.msgList.push(msgObj)
     }
   },
   created () {
@@ -79,3 +79,4 @@ chat-input {
 
 }
 </style>
+
