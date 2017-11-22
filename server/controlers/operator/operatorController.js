9474facb041eba6745fdbe07e7443a5f7e21e6a2
liangjz <liangjz15@mails.tsinghua.edu.cn>
2017-11-23 01:42:20 +0800

    完成前后端客服与客户的接口于controller中，分别为customerController和operatorController，使用时需要先连接二者的eventListener,对上层对象可供使用的函数为initSocket

--- server/controlers/operator/operatorController.js
+++ server/controlers/operator/operatorController.js
@@ -1,13 +1,15 @@
 function operatorControler(){
     this.operators = {};
     this.operatorAccepted = 0;
-    this.event = require('events').EventEmitter();
+    const event = require('events');
+    this.event = new event();
     this.customerListener = null;
     this.operatorAllocator = require('./operatorAllocator');
 
     //设置event处理函数
-    this.event.on('allocate_operator', this.allocateOperator);
-    this.event.on('msg', this.customerMsg.bind(this));
+    this.event.on('allocate_operator', this._allocateOperator);
+    this.event.on('msg', this._customerMsg.bind(this));
+    this.event.on('crash', this._crash.bind(this));
 }
 
 operatorControler.prototype.newSocket = function(socket){
@@ -16,43 +18,44 @@ operatorControler.prototype.newSocket = function(socket){
     socket.handshake.session.tempId = this.operatorAccepted;    //tempId只用来作为在线客服的索引
     this.operatorAllocator.addOperator(this.operatorAccepted);
 
-    socket.on('get_next', this.getNext.bind(this, tmpId));
-    socket.on('msg', this.operatorMsg.bind(this));
-    socket.on('end_service', this.endService.bind(this));
-    socket.on('disconnect', this.disconnect.bind(this, tmpId));
+    socket.on('get_next', this._getNext.bind(this, tmpId));
+    socket.on('msg', this._operatorMsg.bind(this));
+    socket.on('end_service', this._endService.bind(this));
+    socket.on('disconnect', this._disconnect.bind(this, tmpId));
 };
 
 //customerSession中应当保存上一次分配的客服的id,应当在这个函数中完成
 //传来的customerId应该用来找到这个customer
-operatorControler.prototype.allocateOperator = function(customerId, customerSession){
+operatorControler.prototype._allocateOperator = function(customerId, customerSession){
     //TODO:添加熟人优先分配算法
-    var res =  this.operatorAllocator.allocateOperator();
+    var res =  this.operatorAllocator._allocateOperator();
     if(res)
     {
         //分配了有效的客服，则对客服进行通知
         this.operators[res].socket.emit('new_customer', customerId);
         this.operators[res].waitingList.push(customerId);
     }
-    this.customerListener.emit('operator_allocated', res);
+    this.customerListener.emit('operator_allocated',customerId, res);
 };
-operatorControler.prototype.getNext = function(operatorId){
+operatorControler.prototype._getNext = function(operatorId){
     var operator = this.operators[operatorId];
     var nextId = operator.waitingList.shift();
     operator.socket.emit('get_next', nextId);
     operator.chatting.add(nextId);
+    this.customerListener.emit('operator_connected', nextId);
 };
-operatorControler.prototype.customerMsg = function(customerId, operatorId, msg){
+operatorControler.prototype._customerMsg = function(customerId, operatorId, msg){
     this.operators[operatorId].socket.emit('msg', customerId, msg);
 };
-operatorControler.prototype.operatorMsg = function(customerId, msg){
+operatorControler.prototype._operatorMsg = function(customerId, msg){
     this.customerListener.emit('msg', customerId, msg);
 };
-operatorControler.prototype.endService = function(operatorId, customerId){
+operatorControler.prototype._endService = function(operatorId, customerId){
     this.customerListener.emit('end_service', customerId);
     this.operators[operatorId].chatting.delete(customerId);
 };
 //TODO:增加对于operator的状态检查，客服可能处于休息状态
-operatorControler.prototype.disconnect = function(operatorId){
+operatorControler.prototype._disconnect = function(operatorId){
     var operator = this.operators[operatorId];
     var listener = this.customerListener;
     operator.waitingList.forEach(() => {
@@ -61,7 +64,12 @@ operatorControler.prototype.disconnect = function(operatorId){
     operator.chatting.forEach(() => {
         listener.emit('crashed');
     });
+    this.operators[operatorId] = null;
+    this.operatorAllocator.deleteOperator(operatorId);
 };
+operatorControler.prototype._crash = function(operatorId, customerId){
+    this.operators[operatorId].socket.emit('crash', customerId);
+}
 
 //外部使用时应当设置customerListener
 
