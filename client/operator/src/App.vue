807411527bd64d826b88ea1a956f51526257ed15
nloshadow <yit15@mails.tsinghua.edu.cn>
2017-11-23 00:39:30 +0800

    #13 实现了最基础的客服工作台界面，实现了查看当前服务人数、排队人数与服务人数功能，并且实现了接入和休息的按钮（接入按钮还需要进一步进行socket通信）

--- client/operator/src/App.vue
+++ client/operator/src/App.vue
@@ -15,34 +15,18 @@
 </template>
 
 <script>
-
-import ChatComponent from './components/ChatComponent'
-
 export default {
-  components: {
-    ChatComponent
-  }
+  name: 'app'
 }
 </script>
 
 <style>
-*{
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-html {
-  height: 100%;
-}
-
-body {
-  height: 100%;
-}
-.chatall{
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: space-between;
-  width: 600px;
-}
-
 </style>
--- client/operator/src/store.js
+++ client/operator/src/store.js
@@ -4,9 +4,24 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
+  waitingNum: 5,
+  customerNum: 0,
+  todayNum: 3
 }
 
 const mutations = {
+  customerNumChange (state, n) {
+    state.customerNum += n
+  },
+  waitingChange (state, n) {
+    state.waitingNum += n
+  },
+  waitingSet (state, n) {
+    state.waitingNum = n
+  },
+  todayNumUpdate (state, n) {
+    state.todayNum = n
+  }
 }
 
 export default new Vuex.Store({
