fda88fc7070866c1f8b6eef9ce4c0dc0547b0a8d
liangjz <liangjz15@mails.tsinghua.edu.cn>
2017-12-07 00:40:30 +0800

    #32完成了operatorInfo的接口

--- server/models/operatorGroup.js
+++ server/models/operatorGroup.js
@@ -2,7 +2,6 @@ module.exports = (schema, mongoose) => {
     var operatorGroupSchema = new schema({
         name:String,
         sessionCounts:[Number],
-        manualServiceRates:[Number],
         msgCounts:[Number],
         serviceRecordStart:Date,
     });
