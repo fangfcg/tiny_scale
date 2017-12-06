d9f5720aec09ea051b190dba21b171b7b30a57c3
liangjz <liangjz15@mails.tsinghua.edu.cn>
2017-12-06 22:01:18 +0800

    #26修复了测试框架不能正常退出的bug，在after中添加了数据库断开连接和关闭服务器，添加了#25的功能测试

--- server/test/functionTestEntry.js
+++ server/test/functionTestEntry.js
@@ -6,14 +6,17 @@ const path = require('path');
 const basename = path.basename(__filename);
 
 const model = require('../models/models');
+const server = require('../index');
 describe('function test', function(){
-    //在每一个suit结束时清空测试数据库
+    //在每一个suit结束时清空测试数据库以及服务器会话存储
     afterEach(async function(){
         await model.dropDatabase();
+        await server.clearServerState(); 
     });
+    //在全部功能测试结束后清空数据库并结束进程
     after(async function(){
-        await model.dropDatabase();
-        process.exit(0);
+        await model.disconnect();
+        await server.stopServer();
     });
     fs.readdirSync(__dirname)
         .filter(file=>{
