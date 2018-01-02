3a1c771a28f840f12cd9e3a885128037353d56b3
fangfcg <thssss15_fangcg@163.com>
2018-01-03 02:21:34 +0800

    实现了客服和管理员注册的http请求，未测试

--- client/src/components/login/loginSignin.vue
+++ client/src/components/login/loginSignin.vue
@@ -15,14 +15,14 @@
         <div v-else-if="status === 2">
           <div v-if="choosenType===0">
             <div class="normal-input">
-              <el-input placeholder="请输入邮箱号" v-model="username" clearable>
+              <el-input placeholder="请输入邮箱号" v-model="emailNum" clearable>
                 <template slot="prepend">邮箱号</template>
               </el-input>
             </div>
             <div class="normal-input">
-              <el-input placeholder="请输入验证码" v-model="username" clearable>
+              <el-input placeholder="请输入验证码" v-model="certiCode" clearable>
                 <template slot="prepend">验证码</template>
-                <el-button type="primary" slot="append" @click="sendIdenCode">
+                <el-button type="primary" slot="append" @click="sendIdenCode" :disabled="emailSendDisabled">
                 <span>发送验证码</span>
                 </el-button>
               </el-input>
@@ -31,7 +31,7 @@
           </div>
           <div v-if="choosenType===1">
             <div class="normal-input">
-              <el-input placeholder="请输入验证码" v-model="username" clearable>
+              <el-input placeholder="请输入验证码" v-model="inviteCode" clearable>
                 <template slot="prepend">验证码</template>
               </el-input>
               <el-button type="primary" class="next-button" @click="nextStep">下一步</el-button>
@@ -39,6 +39,13 @@
           </div>
         </div>
         <div v-else-if="status === 3">
+          <div v-if="choosenType===0">
+            <div class="normal-input2">
+            <el-input placeholder="请输入公司名称" v-model="companyName" clearable>
+              <template slot="prepend">公司名称</template>
+            </el-input>
+          </div>
+          </div>
           <div class="normal-input2">
             <el-input placeholder="请输入账号" v-model="username" clearable>
               <template slot="prepend">账号</template>
@@ -56,6 +63,12 @@
           </div>
           <el-button type="primary" class="next-button" :disabled="signupFlag" @click="nextStep">下一步</el-button><br>
         </div>
+        <div v-else-if="status === 4">
+          <div>注册成功!</div>
+          <router-link to="/signin">
+            <el-button>返回登录界面</el-button>
+          </router-link>
+        </div>
       </template>
     </div>    
   </div>
@@ -63,20 +76,41 @@
 
 <script>
 
-export default {
+var Login = {
   data () {
     return {
+      certiCode: '',
+      inviteCode: '',
       username: '',
       password: '',
       password2: '',
       usertype: '1',
+      companyName: '',
+      emailNum: '',
       status: 1,      // 1, 2, 3, 4 represents for status respectively
-      choosenType: 0  // 0: admin 1: operator
+      choosenType: 0,  // 0: admin 1: operator
+      emailSendDisabled: false,
+      emailSendFlag: false
     }
   },
   computed: {
     signupFlag () {
       return (this.username === '' || this.password === '' || this.password2 === '') && this.status === 3
+    },
+    sendEmailUrl () {
+      return (this.$store.state.serverIp + '/api/admin/signup/get_certificate')
+    },
+    adminCertificateUrl () {
+      return (this.$store.state.serverIp + '/api/admin/signup/certificate')
+    },
+    operatorCertificateUrl () {
+      return (this.$store.state.serverIp + '/api/operator/signup/certificate')
+    },
+    adminCreateUrl () {
+      return (this.$store.state.serverIp + '/api/admin/signup/create_admin')
+    },
+    operatorCreateUrl () {
+      return (this.$store.state.serverIp + '/api/operator/signup/create_operator')
     }
   },
   methods: {
@@ -91,29 +125,110 @@ export default {
     nextStep () {
       if (this.status === 2) {
         if (this.choosenType === 0) {
-          this.status = 3
+          if (this.certiCode === '') {
+            return
+          }
+          this.$http.post(this.adminCertificateUrl, {
+            certificate: this.certiCode
+          }).then(function (response) {
+            if (response.success === true) {
+              this.status = 3
+            }
+          })
+          if (this.status !== 3) {
+            this.$message.error('验证码错误，请重试')
+          }
         } else {
-          this.status = 3
+          if (this.inviteCode === 0) {
+            return
+          }
+          this.$http.post(this.operatorCertificateUrl, {
+            certificate: this.inviteCode
+          }).then(function (response) {
+            if (response.success === true) {
+              this.status = 3
+            }
+          })
+          if (this.status !== 3) {
+            this.$message.error('验证码错误，请重试')
+          }
         }
         return
       }
       if (this.status === 3) {
         if (this.choosenType === 0) {
-          this.status = 4
+          if (this.companyName === '' || this.password === '' || this.password2 === '' || this.username === '') {
+            return
+          }
+          if (this.password !== this.password2) {
+            this.$message.error('两次密码输入不一致，请重试')
+            return
+          }
+          this.$http.post(this.adminCreateUrl, {
+            companyName: this.companyName,
+            name: this.username,
+            pass: this.password
+          }).then(function (response) {
+            if (response.success === true) {
+              this.status = 4
+            }
+          })
+          if (this.status !== 4) {
+            this.$message.error('注册失败，请重试')
+          }
         } else {
-          this.status = 4
+          if (this.password === '' || this.password2 === '' || this.username === '') {
+            return
+          }
+          if (this.password !== this.password2) {
+            this.$message.error('两次密码输入不一致，请重试')
+            return
+          }
+          this.$http.post(this.operatorCreateUrl, {
+            name: this.username,
+            pass: this.password
+          }).then(function (response) {
+            if (response.success === true) {
+              this.status = 4
+            }
+          })
+          if (this.status !== 4) {
+            this.$message.error('注册失败，请重试')
+          }
         }
       }
     },
     sendIdenCode () {
-      /*
-      this.$http.post(, {
+      if (this.emailNum === '') {
+        return
+      }
+      this.emailSendDisabled = true
+      this.$http.post(this.sendEmailUrl, {
+        email: this.emailNum
       }).then(function (response) {
+        if (response.success === true) {
+          Login.emailSendFlag = true
+        } else {
+          Login.emailSendFlag = false
+        }
+      }).catch(function (error) {
+        console.log(error)
+        Login.emailSendFlag = false
       })
-      */
+      if (this.emailSendFlag === true) {
+        this.$message({
+          message: '发送邮件成功',
+          type: 'success'
+        })
+      } else {
+        this.$message.error('发送邮件失败')
+      }
+      this.emailSendDisabled = false
     }
   }
 }
+
+export default Login
 </script>
 
 <style>
