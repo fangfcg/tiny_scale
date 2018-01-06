24289ddb05299bbef479b5d68351fc4c3183fae1
jcy <jcy15@mails.tsinghua.edu.cn>
2018-01-06 21:26:02 +0800

    对client端的configs.js中的token进行修改

--- client/configs.js
+++ client/configs.js
@@ -1,4 +1,4 @@
 export const urlClient = 'http://183.172.154.239:8000'
 export const urlOperator = 'http://183.172.154.239:8000'
 export const serverIp = 'http://183.172.154.239:8000'
-export const clientToken = 'thu1'
+export const clientToken = 'tsinghua'
